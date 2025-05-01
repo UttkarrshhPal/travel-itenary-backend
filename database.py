1dca9497c38dfa918c9fca78a090526d59bcb715
sravanpant <sravan.pant5@gmail.com>
2025-05-01 22:24:49 +0530

    made changes in database.py

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 # database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://travel_user:123456@localhost/travel_db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
