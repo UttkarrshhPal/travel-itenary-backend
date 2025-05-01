e187f7c14b369bde27dc7ee38840817be2883f17
sravanpant <sravan.pant5@gmail.com>
2025-05-01 22:29:07 +0530

    added frontend url

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Add your frontend URL
+    allow_origins=[
+        "http://localhost:3000",
+        "https://travel-itinerary-frontend-wheat.vercel.app/",
+    ],  # Add your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
